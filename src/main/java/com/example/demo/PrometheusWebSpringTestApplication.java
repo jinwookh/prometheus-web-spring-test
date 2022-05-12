8bd8d780ee95e70108e502d672cebe781dab749b
jinwook han <jin942002@naver.com>
2022-05-12 21:45:17 +0900

    init

---
+++ src/main/java/com/example/demo/PrometheusWebSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrometheusWebSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrometheusWebSpringTestApplication.class, args);
+	}
+
+}
